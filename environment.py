0448d4f257eae83e971a747472e8ce77ac3b709f
Linus C. <linusc9516@gmail.com>
2025-06-27 16:07:00 +0800

    Add files via upload

---
+++ environment.py
@@ -0,0 +1,211 @@
+from physics_simulator import PhysicsSimulator
+from physics_simulator.galbot_interface import GalbotInterface, GalbotInterfaceConfig
+from physics_simulator.utils.data_types import JointTrajectory
+from synthnova_config import PhysicsSimulatorConfig, RobotConfig
+import numpy as np
+
+from pathlib import Path
+
+def interpolate_joint_positions(start_positions, end_positions, steps):
+    return np.linspace(start_positions, end_positions, steps)
+
+class environment():
+    def setup_sim(self):
+        
+        # Create sim config
+        my_config = PhysicsSimulatorConfig()
+
+        # Instantiate the simulator
+        self.simulator = PhysicsSimulator(my_config)
+
+        # Add default ground plane if you need
+        self.simulator.add_default_scene()
+
+        # Add robot
+        self.robot_config = RobotConfig(
+            prim_path="/World/Galbot",
+            name="galbot_one_charlie",
+            mjcf_path=Path()
+            .joinpath(self.simulator.synthnova_assets_directory)
+            .joinpath("synthnova_assets")
+            .joinpath("robot")
+            .joinpath("galbot_one_charlie_description")
+            .joinpath("galbot_one_charlie.xml"),
+            position=[0, 0, 0],
+            orientation=[0, 0, 0, 1]
+        )
+        self.robot_path = self.simulator.add_robot(self.robot_config)
+
+        # Initialize the simulator
+        self.simulator.initialize()
+
+    def init_scene(self):
+        print("scene initialized")
+    
+    def init_pose(self):
+        # Init head pose
+        head = [0.0, 0.0]
+        self._move_joints_to_target(self.interface.head, head)
+
+        # Init leg pose
+        leg = [0.43, 1.48, 1.07, 0.0]
+        self._move_joints_to_target(self.interface.leg, leg)
+
+        # Init left arm pose
+        left_arm = [
+            0.058147381991147995,
+            1.4785659313201904,
+            -0.0999724417924881,
+            -2.097979784011841,
+            1.3999720811843872,
+            -0.009971064515411854,
+            1.0999830961227417,
+        ]
+        self._move_joints_to_target(self.interface.left_arm, left_arm)
+
+        # Init right arm pose
+        right_arm = [
+            -0.058147381991147995,
+            -1.4785659313201904,
+            0.0999724417924881,
+            2.097979784011841,
+            -1.3999720811843872,
+            0.009971064515411854,
+            -1.0999830961227417,
+        ]
+        self._move_joints_to_target(self.interface.right_arm, right_arm)
+    
+    def init_interface(self):
+        galbot_interface_config = GalbotInterfaceConfig()
+
+        galbot_interface_config.robot.prim_path = "/World/Galbot"
+
+        robot_name = "galbot_one_charlie"
+        # Enable modules
+        galbot_interface_config.modules_manager.enabled_modules.append("right_arm")
+        galbot_interface_config.modules_manager.enabled_modules.append("left_arm")
+        galbot_interface_config.modules_manager.enabled_modules.append("leg")
+        galbot_interface_config.modules_manager.enabled_modules.append("head")
+        galbot_interface_config.modules_manager.enabled_modules.append("chassis")
+
+        galbot_interface_config.right_arm.joint_names = [
+            f"{robot_name}/right_arm_joint1",
+            f"{robot_name}/right_arm_joint2",
+            f"{robot_name}/right_arm_joint3",
+            f"{robot_name}/right_arm_joint4",
+            f"{robot_name}/right_arm_joint5",
+            f"{robot_name}/right_arm_joint6",
+            f"{robot_name}/right_arm_joint7",
+        ]
+
+        galbot_interface_config.left_arm.joint_names = [
+            f"{robot_name}/left_arm_joint1",
+            f"{robot_name}/left_arm_joint2",
+            f"{robot_name}/left_arm_joint3",
+            f"{robot_name}/left_arm_joint4",
+            f"{robot_name}/left_arm_joint5",
+            f"{robot_name}/left_arm_joint6",
+            f"{robot_name}/left_arm_joint7",
+        ]
+
+        galbot_interface_config.leg.joint_names = [
+            f"{robot_name}/leg_joint1",
+            f"{robot_name}/leg_joint2",
+            f"{robot_name}/leg_joint3",
+            f"{robot_name}/leg_joint4",
+        ]
+        
+        galbot_interface_config.head.joint_names = [
+            f"{robot_name}/head_joint1",
+            f"{robot_name}/head_joint2"
+        ]
+
+        galbot_interface_config.chassis.joint_names = [
+            f"{robot_name}/mobile_forward_joint",
+            f"{robot_name}/mobile_side_joint",
+            f"{robot_name}/mobile_yaw_joint",
+        ]
+
+        galbot_interface = GalbotInterface(
+            galbot_interface_config=galbot_interface_config,
+            simulator=self.simulator
+        )
+        galbot_interface.initialize()
+
+        self.interface = galbot_interface
+
+    def _move_joints_to_target(self, module, target_positions, steps=200):
+        """Move joints from current position to target position smoothly."""
+        current_positions = module.get_joint_positions()
+        positions = interpolate_joint_positions(current_positions, target_positions, steps)
+        joint_trajectory = JointTrajectory(positions=np.array(positions))
+        module.follow_trajectory(joint_trajectory)
+
+    def follow_path_callback(self):
+        if (len(self.fifoPath) != 0):
+
+            target = self.fifoPath[0]
+            if (self.check_movement_complete(target, 0.1)):
+                self.fifoPath.pop(0)
+                self.moving = False
+
+                if (len(self.fifoPath) == 0):
+                    target = self.fifoPath[0]
+                    self.moveGeneric(target)
+                    self.moving = True
+
+    def moveGeneric(self, target):
+        """Move the robot to a target position."""
+        current_joint_positions = self.interface.chassis.get_joint_positions()
+        target_joint_positions = [target[0], target[1], target[2]]
+        positions = interpolate_joint_positions(
+            current_joint_positions, target_joint_positions, 5000
+        )
+        # Create a joint trajectory
+        joint_trajectory = JointTrajectory(positions=positions)
+
+        # Follow the trajectory
+        self.galbot_interface.chassis.follow_trajectory(joint_trajectory)
+        
+    def main(self):
+
+        self.setup_sim()
+        self.init_interface()
+        # Start the simulation
+
+        self.init_pose()
+        self.init_scene()
+        self.simulator.step()
+
+        # # Get current joint positions
+        # current_joint_positions = self.galbot_interface.chassis.get_joint_positions() # intially [0, 0, 0] ([x,y,yaw], add z in more dimensions)
+
+        # # Define target joint positions
+        # target_joint_positions = [1, 6, 1.5]
+
+        # # Interpolate joint positions
+        # positions = interpolate_joint_positions(
+        #     current_joint_positions, target_joint_positions, 5000
+        # )
+        # # Create a joint trajectory
+        # joint_trajectory = JointTrajectory(positions=positions)
+
+        # # Follow the trajectory
+        # self.galbot_interface.chassis.follow_trajectory(joint_trajectory)
+
+        # action fifo queue
+        self.simulator.add_physics_callback("follow_path_callback", self.follow_path_callback)
+        self.moving = False
+        self.fifoPath = [[0, 0, 0], [1, 6, 1.5], 0]
+
+
+        # Run the display loop
+        while True:
+            self.simulator.step()
+
+        # Close the simulator
+        self.simulator.close()
+
+
+test = environment()
+test.main()
